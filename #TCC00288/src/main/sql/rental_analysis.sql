c1dd44dafb8c10d075cd77a5bc025b6496cb5e5e
Luiz Leme <lapaesleme@gmail.com>
2016-10-21 14:25:17 -0200

    ok

--- #TCC00288/src/main/sql/rental_analysis.sql
+++ #TCC00288/src/main/sql/rental_analysis.sql
@@ -1,9 +1,22 @@
 DROP FUNCTION IF EXISTS rental_analysis();
 CREATE OR REPLACE FUNCTION rental_analysis()
-RETURNS VOID AS '
+RETURNS TABLE (
+  rental_id integer,
+  rental_date timestamp without time zone,
+  due_date timestamp without time zone,
+  replacement_date timestamp without time zone,
+  return_date timestamp without time zone,
+  target_date timestamp without time zone,
+  rental_rate numeric(4,2),
+  late_fee double precision,
+  replacement_cost numeric(5,2),
+  payment_date timestamp without time zone,
+  paid_amount numeric,
+  due_amount double precision,
+  balance double precision) AS '
 DECLARE
 BEGIN
 
-  RETURN;
+    RETURN;
 END;
 ' LANGUAGE plpgsql
