32d9bab8cccf86d553f8b50213114f9f3bb50a49
Luiz Leme <lapaesleme@gmail.com>
2017-05-07 18:37:19 -0300

    ok

---
+++ TIC10002/src/main/java/uff/ic/lleme/tic10002/arvore/Main.java
@@ -0,0 +1,10 @@
+package uff.ic.lleme.tic10002.arvore;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Arvore arvore = new Arvore();
+        No menos = arvore.cria("-");
+
+    }
+}
---
+++ TIC10002/src/main/java/uff/ic/lleme/tic10002/arvore/No.java
@@ -0,0 +1,13 @@
+package uff.ic.lleme.tic10002.arvore;
+
+class No {
+
+    public String conteudo;
+    public No esquerda = null;
+    public No direita = null;
+
+    public No(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+}
