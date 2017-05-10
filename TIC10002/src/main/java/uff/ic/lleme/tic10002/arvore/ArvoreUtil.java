aba92312932da0d6d83ec8da642c6fd2fa52915d
Luiz Leme <lapaesleme@gmail.com>
2017-05-09 22:18:19 -0300

    ok

---
+++ TIC10002/src/main/java/uff/ic/lleme/tic10002/arvore/ArvoreUtil.java
@@ -0,0 +1,114 @@
+package uff.ic.lleme.tic10002.arvore;
+
+public class ArvoreUtil {
+
+    public static class No {
+
+        public String conteudo;
+        private No pai = null;
+        private No esquerda = null;
+        private No direita = null;
+
+        public No(String conteudo) {
+            this.conteudo = conteudo;
+        }
+
+    }
+
+    public static No cria(String conteudo) {
+        return new No(conteudo);
+    }
+
+    public static String visite(No no) {
+        if (no != null)
+            return no.conteudo;
+        else
+            return null;
+    }
+
+    public static No esquerda(No no) {
+        if (no != null)
+            return no.esquerda;
+        else
+            return null;
+    }
+
+    public static No direita(No no) {
+        if (no != null)
+            return no.direita;
+        else
+            return null;
+    }
+
+    public static No pai(No no) {
+        if (no != null)
+            return no.pai;
+        else
+            return null;
+    }
+
+    public static No irmao(No no) {
+        if (no != null && pai(no) != null)
+            if (esquerda(pai(no)) == no)
+                return direita(pai(no));
+            else
+                return esquerda(pai(no));
+        return null;
+    }
+
+    public static boolean eh_esq(No no) {
+        if (no != null && pai(no) != null)
+            return esquerda(pai(no)) == no;
+        else
+            return false;
+    }
+
+    public static boolean eh_dir(No no) {
+        if (no != null && pai(no) != null)
+            return direita(pai(no)) == no;
+        else
+            return false;
+    }
+
+    public static No filho_esq(No no, String conteudo) {
+        if (esquerda(no) == null) {
+            No filho = cria(conteudo);
+            filho.pai = no;
+            no.esquerda = filho;
+            return filho;
+        } else
+            return null;
+    }
+
+    public static No filho_dir(No no, String conteudo) {
+        if (direita(no) == null) {
+            No filho = cria(conteudo);
+            filho.pai = no;
+            no.direita = filho;
+            return filho;
+        } else
+            return null;
+    }
+
+    public static String pre_ordem(No no) {
+        if (no != null)
+            return visite(no) + pre_ordem(direita(no)) + pre_ordem(esquerda(no));
+        else
+            return "";
+    }
+
+    public static String em_ordem(No no) {
+        if (no != null)
+            return em_ordem(direita(no)) + visite(no) + em_ordem(esquerda(no));
+        else
+            return "";
+    }
+
+    public static String pos_ordem(No no) {
+        if (no != null)
+            return pos_ordem(direita(no)) + pos_ordem(esquerda(no)) + visite(no);
+        else
+            return "";
+    }
+
+}
--- TIC10002/src/main/java/uff/ic/lleme/tic10002/arvore/Main.java
+++ TIC10002/src/main/java/uff/ic/lleme/tic10002/arvore/Main.java
@@ -3,8 +3,16 @@ package uff.ic.lleme.tic10002.arvore;
 public class Main {
 
     public static void main(String[] args) {
-        Arvore arvore = new Arvore();
-        No menos = arvore.cria("-");
+        ArvoreUtil.No subtracao = ArvoreUtil.cria("-");
+        ArvoreUtil.No multiplicacao1 = ArvoreUtil.filho_dir(subtracao, "*");
+        ArvoreUtil.No a = ArvoreUtil.filho_dir(multiplicacao1, "a");
+        ArvoreUtil.No b = ArvoreUtil.filho_esq(multiplicacao1, "b");
+        ArvoreUtil.No multiplicacao2 = ArvoreUtil.filho_esq(subtracao, "*");
+        ArvoreUtil.No adicao = ArvoreUtil.filho_dir(multiplicacao2, "+");
+        ArvoreUtil.No f = ArvoreUtil.filho_dir(adicao, "f");
+        ArvoreUtil.No g = ArvoreUtil.filho_esq(adicao, "g");
+        ArvoreUtil.No e = ArvoreUtil.filho_esq(multiplicacao2, "e");
 
+        System.out.println(ArvoreUtil.em_ordem(subtracao));
     }
 }
