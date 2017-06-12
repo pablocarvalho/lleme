c16804461340c28062d4ec1c27de6da2ae2b6c9c
Luiz Leme <lapaesleme@gmail.com>
2017-06-12 07:25:25 -0300

    ok

---
+++ TCC00288/src/main/sql/bonus.sql
@@ -0,0 +1,21 @@
+﻿create or replace function calcula_bonus(p_ano int, p_mat int) 
+returns double as $$
+declare
+    v_bonus double;
+begin
+    v_bonus = 0;
+    with 
+        t1(lucro) as (select valor as lucro
+                      from lucro 
+                      where ano = p_ano);
+        t2(salario) as (select valor as salario
+                        from salario
+                        where matricula = p_matricula)
+    select lucro * 0.01 + salario * 0.05 into v_bonus from t1, t2;
+    if not found then
+        raise notice ''
+    end if
+    return v_bonus;
+end;$$ language plpgsql;
+
+
---
+++ TCC00288/src/main/sql/universidade.sql
@@ -0,0 +1,46 @@
+﻿create or replace function inscricao(p_matricula int
+                                     , p_disciplina int
+                                     , p_turma varchar(2)
+                                     , p_semestre int) 
+returns void as $$
+declare
+    
+begin
+    select limite_turma into v_limturma
+    from Curso
+    where codigo = (select curso from aluno where matricula = p_matricula);
+    
+    if (select count(*)
+        from inscricao 
+        where aluno = p_matricula and semestre = p_semestre) >= v_limturma then
+        raise exception '';
+    end if;
+
+    select limite_alunos into v_limalunos
+    from Disciplina
+    where codigo = p_disciplina
+         
+    if (select count(*)
+        from inscricao 
+        where displina = p_disciplina
+              and turma = p_turma
+              and semestre = p_semestre) >= v_limturma then
+        raise exception '';
+    end if;
+    
+    if exists (with
+                    t1 as (select prerequisito from prerequisito where disciplina = p_disciplina),
+                    t2 as (select disciplina from inscricao where aluno = p_matricula and nf >= 6)
+               select prerequisito
+               from t1 
+                    left join t2 on prerequisito = disciplina 
+                    where disciplina is null)
+               ) then
+        raise exception '';
+    end if
+
+    
+
+    
+end;$$ language plpgsql;
+
