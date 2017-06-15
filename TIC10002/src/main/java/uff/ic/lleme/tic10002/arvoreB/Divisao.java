fa62c95c258e82c90f97ad43d7277572846fc5ff
Luiz Leme <lapaesleme@gmail.com>
2017-06-15 13:12:55 -0300

    ok

--- TIC10002/src/main/java/uff/ic/lleme/tic10002/arvoreB/Divisao.java
+++ TIC10002/src/main/java/uff/ic/lleme/tic10002/arvoreB/Divisao.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package uff.ic.lleme.tic10002.arvoreB;
 
 import uff.ic.lleme.tic10002.Empregado;
 
-/**
- *
- * @author lapaesleme
- */
 public class Divisao {
 
     public Empregado empregado = null;
