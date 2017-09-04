16e17748560d192e8e5a249b8198005bc67b2d0d
Luiz Leme <lapaesleme@gmail.com>
2017-09-04 13:46:10 -0300

    ok

---
+++ TIC10002/src/main/java/uff/ic/lleme/tic10002/aulas/s20172/NewClass.java
@@ -0,0 +1,38 @@
+package uff.ic.lleme.tic10002.aulas.s20172;
+
+public class NewClass {
+
+    public int[] lista = new int[50];
+    public int i = 0;
+    public int j = 0;
+    public int n = 0;
+
+    private void incJ() {
+        if (n < lista.length)
+            j = ++j % lista.length;
+    }
+
+    private void incI() {
+        if (n < lista.length)
+            i = ++i % lista.length;
+    }
+
+    public void incluir(int num) throws Exception {
+        if (n < lista.length) {
+            lista[j] = num;
+            n++;
+            incJ();
+        } else
+            throw new Exception("Capacidade esgotada");
+    }
+
+    public int retirar() throws Exception {
+        if (n > 0) {
+            int num = lista[i];
+            n--;
+            incI();
+            return num;
+        } else
+            throw new Exception("Capacidade esgotada");
+    }
+}
