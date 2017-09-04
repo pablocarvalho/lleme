e67f71d18a25f3ae2f0dd18776b8302bb2ca7482
Luiz Leme <lapaesleme@gmail.com>
2017-09-04 17:24:12 -0300

    ok

---
+++ TIC10002/src/main/java/uff/ic/lleme/tic10002/aulas/s20172/arvore/NoArvore.java
@@ -0,0 +1,10 @@
+package uff.ic.lleme.tic10002.aulas.s20172.arvore;
+
+import uff.ic.lleme.tic10002.aulas.s20172.Empregado;
+
+public class NoArvore {
+
+    public Empregado conteudo;
+    public NoArvore filhoEsquerdo;
+    public NoArvore filhoDireito;
+}
