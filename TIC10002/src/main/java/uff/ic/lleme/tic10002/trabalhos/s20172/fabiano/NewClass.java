6d49125120dce9221954b831563eeea5a8852d4d
lapaesleme <lapaesleme@ic.uff.br>
2017-11-23 14:50:42 -0200

    ok

---
+++ TIC10002/src/main/java/uff/ic/lleme/tic10002/trabalhos/s20172/fabiano/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uff.ic.lleme.tic10002.trabalhos.s20172.fabiano;
+
+/**
+ *
+ * @author lapaesleme
+ */
+public class NewClass {
+
+}
