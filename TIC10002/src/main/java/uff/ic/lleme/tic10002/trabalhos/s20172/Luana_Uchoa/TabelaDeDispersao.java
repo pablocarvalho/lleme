ed3ac821a38fa6696e81b19e818308f590e4e825
luchoa <uchoa.m.luana@gmail.com>
2017-11-24 15:19:20 -0200

    Código do Trabalho (Luana Uchôa)

---
+++ TIC10002/src/main/java/uff/ic/lleme/tic10002/trabalhos/s20172/Luana_Uchoa/TabelaDeDispersao.java
@@ -0,0 +1,32 @@
+package controleTrafego;
+
+import java.util.Iterator;
+
+public class TabelaDeDispersao <E extends Elemento <?, ?>> {
+	
+	ListaEstatica <ListaEncadeada <E>> lista;
+
+    //... construtor.
+    public TabelaDeDispersao (int tamanho) {
+    	lista = new ListaEstatica <ListaEncadeada <E>> (tamanho);
+        
+        for(int i = 0; i < tamanho; i++) {
+        	lista. insere (new ListaEncadeada <E> ());
+        }
+    }
+    
+    private int hash (E obj) {
+    	int chave = obj. getHashCode ();
+    	return chave % (lista. tamanho ());
+    }
+    
+    public E busca (E obj) {
+        return lista. get (hash (obj)). busca (obj);
+    }
+    
+    
+    public void insere (E obj) {
+        lista. get (hash (obj)). insere (obj);
+    }
+    
+}
