7da8d9d539a48aa6593660109db37c36f3de85f0
pablocarvalho <pablomccarvalho@gmail.com>
2018-06-29 20:28:59 -0300

    Esqueci de enviar mudanças na classe par de espera

--- TIC10002/src/main/java/uff/ic/lleme/tic10002/trabalhos/s20181/PabloMoreira/trabalhoed/ParDeEspera.java
+++ TIC10002/src/main/java/uff/ic/lleme/tic10002/trabalhos/s20181/PabloMoreira/trabalhoed/ParDeEspera.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package trabalhoed;
+package uff.ic.lleme.tic10002.trabalhos.s20181.PabloMoreira.trabalhoed;
 
 /**
  *
