6622df8b4437bf8d6473b7a24d7e8cf9022713a2
lapaesleme <lapaesleme@ic.uff.br>
2018-06-29 14:50:58 -0300

    ok

--- TIC10002/src/main/java/uff/ic/lleme/tic10002/trabalhos/s20181/LuizAntonio_e_CarlosMuniz/HashMap.java
+++ TIC10002/src/main/java/uff/ic/lleme/tic10002/trabalhos/s20181/LuizAntonio_e_CarlosMuniz/HashMap.java
@@ -4,12 +4,6 @@
  * and open the template in the editor.
  */
 package uff.ic.lleme.tic10002.trabalhos.s20181.LuizAntonio_e_CarlosMuniz;
-
-import java.util.Random;
-import model.Atendimento;
-import model.TipoAssunto;
-
-// Com a tabela Hash ficaria assim: você passa o tipo de assunto e a função consulta a tabela que te retorna uma lista 
 // encadeada de todos os atendimentos encerrados que possuem assuntos daquele tipo.
 
 /**
@@ -17,112 +11,87 @@ import model.TipoAssunto;
  * @author Carlos Muniz
  */
 public class HashMap {
-	
-	public class No
-	{
-		private Atendimento elemento;
-		private No proximo;
-		
-		public No( Atendimento elemento, No proximo )
-		{
-			this.elemento = elemento;
-			this.proximo = proximo;
-		}
-		
-		public Atendimento getElemento()
-		{
-			return this.elemento;
-		}
-		
-		public void setElemento( Atendimento elemento )
-		{
-			this.elemento = elemento;
-		}
-		
-		public No getProximo ()
-		{
-			return this.proximo;			
-		}
-		
-		public void setProximo( No proximo )
-		{
-			this.proximo = proximo;
-		}
-		
-	}
-	
-	private int tamanho = 11;
-	private No[] tabela = new No[tamanho]; 
- 
-	public HashMap()
-	{
-		for (int i = 0; i < tamanho; i++)
-		{
-			tabela[i] = null;
-		}
-		
-	}
-	
-	public void incluir(Atendimento atendimento, TipoAssunto tipo)
-	{
-		int i = hash(tipo.getTipo());
-		if (tabela[i] == null)
-		{
-			tabela[i] = new No(atendimento, null);
-		}
-		else
-		{
-			No atual = tabela[i];
-			if (!tabela[i].getElemento().equals(atendimento))
-			{
-				while (atual.getProximo() != null)
-				{
-					atual = atual.getProximo();
-					if (atual.getElemento().equals(atendimento))
-					{
-						return;
-					}					
-				}
-				atual.setProximo(new No(atendimento, null));				
-			}					
-		}
-	}
 
-	public void incluir(Atendimento atendimento)
-	{
-		if( atendimento != null )
-		{
-			Assunto[] assuntos = atendimento.getAssuntos();
-			
-			for( Assunto assunto : assuntos )
-			{
-				incluir(atendimento, assunto.getTipo());
-			}
-			
-		}
-		
-	}
-	
-	public No consultar(TipoAssunto tipo)
-	{
-		int i = hash(tipo.getTipo());
-		return tabela[i];
-	}
-	
-	private int hash(String nome)
-	{
-		int h = 0;
-		int a = 31;
-		for (int i=0 ; i < nome.length(); ++i)
-		{
-			h = ((h*a) + nome.charAt(i)) % tamanho;
-		}
-		return h;
-	}
-	
-	public int getTamanho()
-	{
-		return this.tamanho;
-	}
-	
+    public class No {
+
+        private Atendimento elemento;
+        private No proximo;
+
+        public No(Atendimento elemento, No proximo) {
+            this.elemento = elemento;
+            this.proximo = proximo;
+        }
+
+        public Atendimento getElemento() {
+            return this.elemento;
+        }
+
+        public void setElemento(Atendimento elemento) {
+            this.elemento = elemento;
+        }
+
+        public No getProximo() {
+            return this.proximo;
+        }
+
+        public void setProximo(No proximo) {
+            this.proximo = proximo;
+        }
+
+    }
+
+    private int tamanho = 11;
+    private No[] tabela = new No[tamanho];
+
+    public HashMap() {
+        for (int i = 0; i < tamanho; i++)
+            tabela[i] = null;
+
+    }
+
+    public void incluir(Atendimento atendimento, TipoAssunto tipo) {
+        int i = hash(tipo.getTipo());
+        if (tabela[i] == null)
+            tabela[i] = new No(atendimento, null);
+        else {
+            No atual = tabela[i];
+            if (!tabela[i].getElemento().equals(atendimento)) {
+                while (atual.getProximo() != null) {
+                    atual = atual.getProximo();
+                    if (atual.getElemento().equals(atendimento))
+                        return;
+                }
+                atual.setProximo(new No(atendimento, null));
+            }
+        }
+    }
+
+    public void incluir(Atendimento atendimento) {
+        if (atendimento != null) {
+            Assunto[] assuntos = atendimento.getAssuntos();
+
+            for (Assunto assunto : assuntos)
+                incluir(atendimento, assunto.getTipo());
+
+        }
+
+    }
+
+    public No consultar(TipoAssunto tipo) {
+        int i = hash(tipo.getTipo());
+        return tabela[i];
+    }
+
+    private int hash(String nome) {
+        int h = 0;
+        int a = 31;
+        for (int i = 0; i < nome.length(); ++i)
+            h = ((h * a) + nome.charAt(i)) % tamanho;
+        return h;
+    }
+
+    public int getTamanho() {
+        return this.tamanho;
+    }
+
 }
--- TIC10002/src/main/java/uff/ic/lleme/tic10002/trabalhos/s20181/LuizAntonio_e_CarlosMuniz/ServicoDeAtendimento.java
+++ TIC10002/src/main/java/uff/ic/lleme/tic10002/trabalhos/s20181/LuizAntonio_e_CarlosMuniz/ServicoDeAtendimento.java
@@ -8,121 +8,103 @@ package uff.ic.lleme.tic10002.trabalhos.s20181.LuizAntonio_e_CarlosMuniz;
 import java.util.Calendar;
 import java.util.Random;
 
-import model.Assunto;
-import model.Atendimento;
-import model.Cliente;
-import model.HashMap;
-import model.Heap;
-import model.ListaDeAtendimentos;
-import model.ListaDeAtendimentosEncerrados;
-import model.TipoAssunto;
-
 /**
  *
  * @author Luiz Antonio
  */
 public class ServicoDeAtendimento {
-	
-	public static Heap heap = new Heap();
-	public static HashMap hashMap = new HashMap();
-	
-	public static ListaDeAtendimentos atendimentos = new ListaDeAtendimentos();
-	public static ListaDeAtendimentosEncerrados encerrados = new ListaDeAtendimentosEncerrados();
-	
-	public static void recepcionar( Cliente cliente, Assunto[] assuntos )
-	{
-		Calendar horaChegada = Calendar.getInstance();
-		Atendimento atendimento = new Atendimento( cliente, assuntos, horaChegada );
-		heap.insert( atendimento );
-	}
-	
-	public static Atendimento atender()
-	{
-		Calendar horaAtendimento = Calendar.getInstance();
-		Atendimento atendimento = heap.remove( horaAtendimento );
-		atendimento.setHoraAtendimento( horaAtendimento );
-		atendimentos.incluir( atendimento );
-		return atendimento;
-	}
-	
-	public static void encerrar( int idCliente )
-	{
-		Calendar horaEncerramento = Calendar.getInstance();
-		
-		Atendimento atendimento = atendimentos.buscar( idCliente );
-		if( atendimento != null )
-		{
-			Calendar horaAtendimento = atendimento.getHoraAtendimento();
-			
-			// duração do atendimento em minutos
-			int duracao = calculaDuracaoAtendimento( horaAtendimento, horaEncerramento );
-			
-			Assunto[] assuntos = atendimento.getAssuntos();
-			
-			Random random = new Random();
-			for( Assunto assunto : assuntos )
-			{
-				// Configura a duração em minutos de cada assunto e as providências. É apenas uma simplificação
-				int duracaoAssunto = random.nextInt( 180 ) + 1;
-				assunto.setDuracaoAtendimento( duracaoAssunto );
-				assunto.setProvidencias("Voltar daqui a 3 meses para reavaliação");
-			}
-			
-			encerrados.incluir( atendimento );
-			hashMap.incluir( atendimento );
-		}
-	}
-	
-	public static double gerarEstatistica( TipoAssunto tipo )
-	{
-		Calendar dataAtendimento = Calendar.getInstance();
-		double tempoMedio = encerrados.calculaTempoMedioDeAtendimento( tipo, dataAtendimento );
-		return tempoMedio;
-	}
-	
-	/**
-	 * Calcula a duração do atendimento em minutos
-	 * @param inicio
-	 * @param fim
-	 * @return
-	 */
-	private static int calculaDuracaoAtendimento( Calendar inicio, Calendar fim )
-	{
-		int startHour = inicio.get( Calendar.HOUR_OF_DAY );
-		int startMinutes = inicio.get( Calendar.MINUTE );
-		
-		int endHour = fim.get( Calendar.HOUR_OF_DAY );
-		int endMinutes = fim.get( Calendar.MINUTE );
-		
-		int startDay = inicio.get( Calendar.DAY_OF_MONTH );
-		int startMonth = inicio.get( Calendar.MONTH );
-		
-		int endDay = fim.get( Calendar.DAY_OF_MONTH );
-		int endMonth = fim.get( Calendar.MONTH );
-		
-		// situação onde o atendimento se encerrou após o dia de início
-		if( ( endDay > startDay ) || ( endMonth >= startMonth ) )
-		{
-			/**
-			 * Para simplificação da lógica, caso um atendimento tenha sido encerrado X dias após o dia em que o mesmo foi iniciado,
-			 * assume-se sempre o valor 1 para X.
-			 */
-			endHour += 24;
-		}
-		
-		int diffHour = endHour - startHour;
-		int diffMinutes = endMinutes - startMinutes;
-		
-		if( endMinutes < startMinutes )
-		{
-			diffHour --;
-			int newEndMinutes = endMinutes + 60;
-			diffMinutes = newEndMinutes - startMinutes;
-		}
-		
-		int hoursIntoMinutes = diffHour * 60;
-		int duracaoAtendimento = hoursIntoMinutes + diffMinutes;
-		return duracaoAtendimento;		
-	}
+
+    public static Heap heap = new Heap();
+    public static HashMap hashMap = new HashMap();
+
+    public static ListaDeAtendimentos atendimentos = new ListaDeAtendimentos();
+    public static ListaDeAtendimentosEncerrados encerrados = new ListaDeAtendimentosEncerrados();
+
+    public static void recepcionar(Cliente cliente, Assunto[] assuntos) {
+        Calendar horaChegada = Calendar.getInstance();
+        Atendimento atendimento = new Atendimento(cliente, assuntos, horaChegada);
+        heap.insert(atendimento);
+    }
+
+    public static Atendimento atender() {
+        Calendar horaAtendimento = Calendar.getInstance();
+        Atendimento atendimento = heap.remove(horaAtendimento);
+        atendimento.setHoraAtendimento(horaAtendimento);
+        atendimentos.incluir(atendimento);
+        return atendimento;
+    }
+
+    public static void encerrar(int idCliente) {
+        Calendar horaEncerramento = Calendar.getInstance();
+
+        Atendimento atendimento = atendimentos.buscar(idCliente);
+        if (atendimento != null) {
+            Calendar horaAtendimento = atendimento.getHoraAtendimento();
+
+            // duração do atendimento em minutos
+            int duracao = calculaDuracaoAtendimento(horaAtendimento, horaEncerramento);
+
+            Assunto[] assuntos = atendimento.getAssuntos();
+
+            Random random = new Random();
+            for (Assunto assunto : assuntos) {
+                // Configura a duração em minutos de cada assunto e as providências. É apenas uma simplificação
+                int duracaoAssunto = random.nextInt(180) + 1;
+                assunto.setDuracaoAtendimento(duracaoAssunto);
+                assunto.setProvidencias("Voltar daqui a 3 meses para reavaliação");
+            }
+
+            encerrados.incluir(atendimento);
+            hashMap.incluir(atendimento);
+        }
+    }
+
+    public static double gerarEstatistica(TipoAssunto tipo) {
+        Calendar dataAtendimento = Calendar.getInstance();
+        double tempoMedio = encerrados.calculaTempoMedioDeAtendimento(tipo, dataAtendimento);
+        return tempoMedio;
+    }
+
+    /**
+     * Calcula a duração do atendimento em minutos
+     *
+     * @param inicio
+     * @param fim
+     * @return
+     */
+    private static int calculaDuracaoAtendimento(Calendar inicio, Calendar fim) {
+        int startHour = inicio.get(Calendar.HOUR_OF_DAY);
+        int startMinutes = inicio.get(Calendar.MINUTE);
+
+        int endHour = fim.get(Calendar.HOUR_OF_DAY);
+        int endMinutes = fim.get(Calendar.MINUTE);
+
+        int startDay = inicio.get(Calendar.DAY_OF_MONTH);
+        int startMonth = inicio.get(Calendar.MONTH);
+
+        int endDay = fim.get(Calendar.DAY_OF_MONTH);
+        int endMonth = fim.get(Calendar.MONTH);
+
+        // situação onde o atendimento se encerrou após o dia de início
+        if ((endDay > startDay) || (endMonth >= startMonth))
+            /**
+             * Para simplificação da lógica, caso um atendimento tenha sido encerrado X dias após o dia em que
+             * o mesmo foi iniciado, assume-se sempre o valor 1 para X.
+             */
+            endHour += 24;
+
+        int diffHour = endHour - startHour;
+        int diffMinutes = endMinutes - startMinutes;
+
+        if (endMinutes < startMinutes) {
+            diffHour--;
+            int newEndMinutes = endMinutes + 60;
+            diffMinutes = newEndMinutes - startMinutes;
+        }
+
+        int hoursIntoMinutes = diffHour * 60;
+        int duracaoAtendimento = hoursIntoMinutes + diffMinutes;
+        return duracaoAtendimento;
+    }
 
 }
